ae590ddc00b9642882579fe028df373de9bc47b0
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2018-12-02 14:25:12 +0530

    Update system-setup.sh

--- system-setup.sh
+++ system-setup.sh
@@ -7,6 +7,8 @@ curl https://www.apache.org/dist/cassandra/KEYS | sudo apt-key add -
 apt update
 apt upgrade
 
+apt install nodejs
+
 apt install mariadb-server
 
 apt install openjdk-8-jdk
@@ -25,3 +27,11 @@ systemctl start cassandra
 wget http://packages.confluent.io/archive/5.0/confluent-oss-5.0.1-2.11.tar.gz
 tar xf confluent-oss-5.0.1-2.11.tar.gz
 rm confluent-oss-5.0.1-2.11.tar.gz
+
+wget http://mirrors.fibergrid.in/apache/spark/spark-2.3.2/spark-2.3.2-bin-hadoop2.7.tgz
+
+tar xf spark-2.3.2-bin-hadoop2.7.tgz
+
+wget http://mirrors.wuchna.com/apachemirror/hadoop/common/hadoop-2.7.7/hadoop-2.7.7.tar.gz
+
+tar xf hadoop-2.7.7.tar.gz
