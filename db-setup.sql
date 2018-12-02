b92d6007d5fe949c4b0e929fb9c2510fd771a9ef
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2018-12-02 13:39:05 +0530

    Create db-setup.sql

---
+++ db-setup.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE workshop;
+USE workshop;
+
+
+CREATE TABLE users (id varchar(42) primary key, fullname varchar(250), gender varchar(10), email varchar(100), phone varchar(20), state varchar(100), country varchar(50), membership varchar(20));
