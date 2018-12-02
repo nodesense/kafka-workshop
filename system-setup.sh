ca6ac5e30c3299241a77cd751030da44ee06fcef
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2018-12-02 13:20:27 +0530

    Create system-setup.sh

---
+++ system-setup.sh
@@ -0,0 +1,26 @@
+
+
+
+echo "deb http://www.apache.org/dist/cassandra/debian 311x main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
+curl https://www.apache.org/dist/cassandra/KEYS | sudo apt-key add -
+
+apt update
+apt upgrade
+
+apt install mariadb-server
+
+apt install openjdk-8-jdk
+
+apt install python -y
+
+apt  install python-setuptools 
+apt install python-pip
+pip install cassandra-driver
+
+apt install cassandra
+systemctl enable cassandra
+systemctl start cassandra
+
+
+wget http://packages.confluent.io/archive/5.0/confluent-oss-5.0.1-2.11.tar.gz
+tar xf http://packages.confluent.io/archive/5.0/confluent-oss-5.0.1-2.11.tar.gz
