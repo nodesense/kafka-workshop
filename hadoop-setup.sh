0e4525340874a0d4b3df566a28a5f7fb9fea6eac
cloud@nodesense.ai <cloud@nodesense.ai>
2018-12-05 19:53:43 +0530

    hdfs files

--- hadoop-setup.sh
+++ hadoop-setup.sh
@@ -3,7 +3,7 @@ yes | cp -rf hadoop/core-site.xml $HADOOP_HOME/etc/hadoop/core-site.xml
 yes | cp -rf hadoop/mapred-site.xml $HADOOP_HOME/etc/hadoop/mapred-site.xml
 yes | cp -rf hadoop/yarn-site.xml $HADOOP_HOME/etc/hadoop/yarn-site.xml
 
-mkdirp /data/hdfs
+mkdir -p /data/hdfs
 
 chmod -R 777 /data/hdfs
 
