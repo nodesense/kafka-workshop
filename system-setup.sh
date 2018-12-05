65cb54f42b3a49b015c4a9926fbe7ded3f25bb1c
cloud@nodesense.ai <cloud@nodesense.ai>
2018-12-05 23:39:50 +0530

    System update

--- system-setup.sh
+++ system-setup.sh
@@ -13,7 +13,7 @@ curl https://www.apache.org/dist/cassandra/KEYS | sudo apt-key add -
 
 apt install nodejs  -y
 
-apt install mariadb-server  -y
+apt install mysql-server  -y
 
 apt install openjdk-8-jdk -y
 
@@ -47,8 +47,6 @@ echo "HADOOP_COMMON_LIB_NATIVE_DIR=/$USER/hadoop-2.7.7/lib/native" >>  /etc/envi
 echo "KAFKA_HOME=/$USER/confluent-5.0.1" >> /etc/environment
 
 echo "SPARK_HOME=/$USER/spark-2.3.2-bin-hadoop2.7" >> /etc/environment
-
-
 
 
 echo "export PATH=\$PATH:\$HADOOP_HOME/sbin:\$HADOOP_HOME/bin" >>  ~/.profile
