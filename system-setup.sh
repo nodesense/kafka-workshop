1b37f8da42f63b482ba9ba4df67e1595e0850bcb
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2020-01-22 23:43:05 +0530

    Update system-setup.sh

--- system-setup.sh
+++ system-setup.sh
@@ -78,6 +78,17 @@ export JRE_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 export HADOOP_HOME=/$USER/hadoop-2.7.7
 
 
+mv $HADOOP_HOME/etc/hadoop/core-site.xml $HADOOP_HOME/etc/hadoop/core-site.xml.original
+mv $HADOOP_HOME/etc/hadoop/hdfs-site.xml $HADOOP_HOME/etc/hadoop/hdfs-site.xml.original
+mv $HADOOP_HOME/etc/hadoop/mapred-site.xml $HADOOP_HOME/etc/hadoop/mapred-site.xml.original
+mv $HADOOP_HOME/etc/hadoop/yarn-site.xml $HADOOP_HOME/etc/hadoop/yarn-site.xml.original
+
+
+wget -P $HADOOP_HOME/etc/hadoop/ https://raw.githubusercontent.com/nodesense/kafka-workshop/master/hadoop/core-site.xml
+wget -P $HADOOP_HOME/etc/hadoop/ https://raw.githubusercontent.com/nodesense/kafka-workshop/master/hadoop/hdfs-site.xml
+wget -P $HADOOP_HOME/etc/hadoop/ https://raw.githubusercontent.com/nodesense/kafka-workshop/master/hadoop/mapred-site.xml
+wget -P $HADOOP_HOME/etc/hadoop/ https://raw.githubusercontent.com/nodesense/kafka-workshop/master/hadoop/yarn-site.xml
+
 mkdir -p /data/hdfs
 #chown hduser:hadoop  /data/hadoop
 chmod 777 /data/hdfs
