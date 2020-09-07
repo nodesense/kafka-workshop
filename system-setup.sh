78aef9385d01f1293cb1d6f74430785442ac64c6
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2020-09-08 00:53:15 +0530

    Update system-setup.sh

--- system-setup.sh
+++ system-setup.sh
@@ -36,7 +36,7 @@ echo "YARN_HOME=/$USER/hadoop-2.7.7" >>  /etc/environment
 
 echo "HADOOP_COMMON_LIB_NATIVE_DIR=/$USER/hadoop-2.7.7/lib/native" >>  /etc/environment
  
-echo "KAFKA_HOME=/$USER/confluent-5.2.2" >> /etc/environment
+echo "KAFKA_HOME=/$USER/confluent-5.5.1" >> /etc/environment
 
 echo "SPARK_HOME=/$USER/spark-2.3.2-bin-hadoop2.7" >> /etc/environment
 
@@ -49,13 +49,14 @@ apt install mysql-server  -y
 apt install apt-transport-https -y
 
 
-wget http://packages.confluent.io/archive/5.2/confluent-5.2.2-2.11.tar.gz
-tar xf confluent-5.2.2-2.11.tar.gz
+
+wget http://packages.confluent.io/archive/5.5/confluent-5.5.1-2.12.tar.gz
+tar xf confluent-5.5.1-2.12.tar.gz
 
 wget https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.47.tar.gz
 
 tar xf mysql-connector-java-5.1.47.tar.gz
-cp mysql-connector-java-5.1.47/*.jar confluent-5.2.2/share/java/kafka-connect-jdbc
+cp mysql-connector-java-5.1.47/*.jar confluent-5.5.1/share/java/kafka-connect-jdbc
 
 
 wget http://apache.cs.utah.edu/spark/spark-2.3.2/spark-2.3.2-bin-hadoop2.7.tgz
