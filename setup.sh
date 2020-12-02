067bd7252792f7e0f45e62acd6b2c3c519b56c6e
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2020-12-02 20:16:11 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,8 +7,24 @@ apt update
 
 apt install openjdk-8-jdk -y
 
-echo "JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64" >> /etc/environment
-echo "JRE_HOME=/usr/lib/jvm/java-8-openjdk-amd64" >> /etc/environment 
+wget https://archive.apache.org/dist/hadoop/common/hadoop-2.7.7/hadoop-2.7.7.tar.gz
+
+wget https://downloads.apache.org/hive/hive-2.3.7/apache-hive-2.3.7-bin.tar.gz
+
+
+tar xf hadoop-2.7.7.tar.gz
+
+tar xf apache-hive-2.3.7-bin.tar.gz
+mv apache-hive-2.3.7-bin apache-hive-2.3.7
+
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export JRE_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export HADOOP_HOME=/$USER/hadoop-2.7.7
+export HIVE_HOME=/$USER/apache-hive-2.3.7
+
+
+echo "JAVA_HOME=$JAVA_HOME" >> /etc/environment
+echo "JRE_HOME=$JRE_HOME" >> /etc/environment 
 
 echo "HADOOP_HOME=/$USER/hadoop-2.7.7" >>  /etc/environment
 echo "HADOOP_INSTALL=/$USER/hadoop-2.7.7" >>  /etc/environment
@@ -21,24 +37,6 @@ echo "HADOOP_COMMON_LIB_NATIVE_DIR=/$USER/hadoop-2.7.7/lib/native" >>  /etc/envi
 echo "export PATH=\$PATH:\$HADOOP_HOME/sbin:\$HADOOP_HOME/bin" >>  ~/.profile
 
 echo "HIVE_HOME=/$USER/apache-hive-2.3.7" >>  /etc/environment
-
-
-wget https://archive.apache.org/dist/hadoop/common/hadoop-2.7.7/hadoop-2.7.7.tar.gz
-
-tar xf hadoop-2.7.7.tar.gz
-
-wget https://downloads.apache.org/hive/hive-2.3.7/apache-hive-2.3.7-bin.tar.gz
-tar xf apache-hive-2.3.7-bin.tar.gz
-mv apache-hive-2.3.7-bin apache-hive-2.3.7
-
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-export JRE_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-
-export HADOOP_HOME=/$USER/hadoop-2.7.7
-
-export HIVE_HOME=/$USER/apache-hive-2.3.7
 
 mv $HADOOP_HOME/etc/hadoop/core-site.xml $HADOOP_HOME/etc/hadoop/core-site.xml.original
 mv $HADOOP_HOME/etc/hadoop/hdfs-site.xml $HADOOP_HOME/etc/hadoop/hdfs-site.xml.original
