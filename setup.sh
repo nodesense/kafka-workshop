db08f8fffef3f335892e6691c6187094cbad7758
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2020-12-03 17:51:12 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -85,15 +85,3 @@ chmod 777 /data/hdfs
 mkdir -p  /data/derby
 chmod 777 /data/derby
 
-hdfs namenode -format
-
-$HADOOP_HOME/sbin/start-all.sh
-
-$HADOOP_HOME/bin/hadoop fs -mkdir /tmp 
-$HADOOP_HOME/bin/hadoop fs -mkdir /user
-
-$HADOOP_HOME/bin/hadoop fs -mkdir /user/hive
-
-$HADOOP_HOME/bin/hadoop fs -mkdir /user/hive/warehouse
-$HADOOP_HOME/bin/hadoop fs -chmod g+w /tmp 
-$HADOOP_HOME/bin/hadoop fs -chmod g+w /user/hive/warehouse
